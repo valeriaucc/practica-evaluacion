d765f043b669be76dc90d81ab0d53cb15c39a058
Valeria Torres <valeria.gongora@campusucc.edu.co>
2026-05-05 19:58:56 -0500

    subiendo modelo

--- evaluaciones/calificaciones/models.py
+++ evaluaciones/calificaciones/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+class Calificacion(models.Model):
+    nombre_estudiante = models.CharField(max_length=150)
+    identificacion = models.CharField(max_length=15)
+    asignatura = models.CharField(max_length=100)
+    
+    nota1 = models.DecimalField(max_digits=5, decimal_places=2)
+    nota2 = models.DecimalField(max_digits=5, decimal_places=2)
+    nota3 = models.DecimalField(max_digits=5, decimal_places=2)
+    
+    promedio = models.DecimalField(max_digits=5, decimal_places=2, editable=False)
+
+    def calcular_promedio(self):
+        return round((self.nota1 + self.nota2 + self.nota3) / 3, 2)
+
+    def save(self, *args, **kwargs):
+        self.promedio = self.calcular_promedio()
+        super().save(*args, **kwargs)
+
+    def __str__(self):
+        return self.nombre_estudiante
